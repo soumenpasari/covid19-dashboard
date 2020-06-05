407a426148f673b36468cc141f0e4a6fc121d133
soumenpasari <soumenpasari849@gmail.com>
2020-06-05 19:05:57 +0530

    feat:added fontawesome module

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React, {Fragment} from 'react';
 import './App.css';
 import 'bulma/css/bulma.min.css'
 
+// font awesome component and creating a library
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 // importing components
 import {Navbar} from './components/Navbar';
+
+library.add(faCheckSquare, faCoffee)
 
 function App() {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import PropTypes from  'prop-types'
+import PropTypes from  'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export const Navbar = ({title}) => {
     return (
         <nav className='navbar is-dark'>
             <div className='container'>
                 <div className='navbar-brand'>
+                    <span className='navbar-item'>
+                        <FontAwesomeIcon icon='coffee' />
+                    </span>
                     <span className='navbar-item'>
                         {title}
                     </span>
