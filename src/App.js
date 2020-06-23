69b7a0ded5f2a99775d113d11f62b909e8a32f6c
soumenpasari <soumenpasari849@gmail.com>
2020-06-23 14:15:41 +0530

    feat:about page added

--- src/App.js
+++ src/App.js
@@ -7,14 +7,17 @@ import 'bulma/css/bulma.min.css';
 import Store from './context/Store';
 
 // font awesome component and creating a library
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { library } from '@fortawesome/fontawesome-svg-core';
 import { faVirus } from '@fortawesome/free-solid-svg-icons';
+import { faRocket } from '@fortawesome/free-solid-svg-icons';
 
 // importing components
 import {Navbar} from './components/Navbar';
 import {Dashboard} from './components/Dashboard';
+import { About } from './components/About';
 
-library.add(faVirus)
+library.add(faVirus);
+library.add(faRocket);
 
 function App() {
   return (
@@ -25,6 +28,11 @@ function App() {
           <Route exact path='/' render={props => (
             <Fragment>
               <Dashboard />
+            </Fragment>
+          )} />
+          <Route exact path='/about' render={props => (
+            <Fragment>
+              <About />
             </Fragment>
           )} />
         </Switch>
